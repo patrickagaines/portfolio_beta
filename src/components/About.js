46a5ab408f64a694a5a5c4cff80140c0273e88a0
Patrick Gaines <patrickg.codes@gmail.com>
2020-09-09 18:24:21 -0500

    Finished Relay Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useContext} from 'react';
 import { MyContext } from '../context/MyContext.js';
 import Footer from './Footer';
-import Collage from '../images/logocollagetest4.png';
+import Collage from '../images/logocollage.png';
 import CompleteCare from '../images/completecarecartoon.jpeg';
 
 const About = () => {
@@ -15,19 +15,19 @@ const About = () => {
         });
     }, [setPage]);
 
-    const technologyBody = "Full-stack web developer specializing in implementing React and providing fluid, front end user experience. Deep knowledge of design and user interface. Skilled in creating production level REST and GraphQL APIs. Accomplished at creating fully functional, secure ecommerce web sites. Certified AWS Cloud Practitioner with comprehension of both traditional and serverless cloud platforms. Experienced in Python, Django, and MTV Framework. Proficient in Photoshop."
-
-    const automotiveBody = "Six years of experience as a full-time mechanic. What began with simple brake jobs, tune-ups, and suspension repairs, quickly grew into a passion and eagerness to hone and perfect my craft. I became proficient in advanced electrical testing and diagnostics as well as custom wiring modifications. Successfully completed engine rebuilds, exhaust fabrications, and emissions repairs. Programmed and modified Engine Control Modules by updating volumetric efficiency tables, ignition timing parameters, and target lambda air-fuel ratios. My career as a technician taught me the invaluable skill of learning a new system or component, and breaking down its functions for troubleshooting, repair, and improvement."
+    const technologyBody = "Full-stack web developer specializing in React and providing fluid, front end user experience. Deep knowledge of design and user interface. Skilled in creating production level REST and GraphQL APIs. Accomplished at creating fully functional, secure eCommerce web sites. Certified AWS Cloud Practitioner with the ability to provision and manage both traditional and serverless cloud platforms. Experienced in Python, Django, and MTV Framework. Proficient in Photoshop."
 
     const realEstateBody = "Currently licensed Real Estate Broker and an active member of the Chicago Association of Realtors. Specializing in residential leasing, I work directly with the vast majority of property management companies on the north side of Chicago as well as many private owners. My process begins with obtaining a solid understanding of my clients’ wants and needs, and continues through finding them the perfect home, to negotiating the final details of their offer and contract. My role as an agent has pushed me to develop my skills in listening, communication, and consultation. It has also awarded me a newfound appreciation for my city and the people who live here. I continue to work towards further growth in the field and am excited to see what lies in the future."
+
+    const automotiveBody = "Six years of experience as a full-time mechanic. What began with simple brake jobs, tune-ups, and suspension repairs, quickly grew into a passion and eagerness to hone and perfect my craft. I became proficient in advanced electrical testing and diagnostics as well as custom wiring modifications. Successfully completed engine rebuilds, exhaust fabrications, and emissions repairs. Programmed and modified Engine Control Modules by updating volumetric efficiency tables, ignition timing parameters, and target lambda air-fuel ratios. My career as a technician taught me the invaluable skill of learning a new system or component, and breaking down its functions for troubleshooting, repair, and improvement."
 
     return(
         <div className="container-fluid dashboard-body">
             <div className="about-me">
                 <div className="row about-section align-items-center">
                     <div className="col-xsm-12 col-md-6 about-text">
-                        <h2 className="about-greeting">Hello,</h2>
-                        <h2 className="about-greeting greeting-bottom">I'm Patrick</h2>
+                        <h2 className="about-greeting">Hi,</h2>
+                        <h2 className="about-greeting greeting-bottom">I'm Patrick.</h2>
                         <h2>Web Development</h2>
                         <p>{technologyBody}</p>
                     </div>
--- src/components/RelayForJustice.js
+++ src/components/RelayForJustice.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useContext } from 'react';
 import { MyContext } from '../context/MyContext.js';
 import { NavLink } from 'react-router-dom';
+import { ExternalLink } from 'react-external-link';
 import Footer from './Footer';
-import RelayLogo from '../images/Relayforjustice.png';
+import RelayBanner from '../images/Relayforjustice.png';
+import RFJdesktop from '../images/RFJdesktop.png';
+import RFJmobile1 from '../images/RFJmobile1.png';
+import RFJmobile3 from '../images/RFJmobile3.png';
+import RFJmobile4 from '../images/RFJmobile4.png';
+import RFJmobile5 from '../images/RFJmobile5.png';
+import RFJtestimonial from '../images/RFJtestimonial.png';
 
 const RelayForJustice = () => {
     const { setPage } = useContext(MyContext);
@@ -15,6 +22,10 @@ const RelayForJustice = () => {
         });
     }, [setPage]);
 
+    const textArea1 = "As the front-end engineer for the mobile version of our application, I was responsible for both the design, and development of the mobile interface. Focused on user interaction and application performance, Relay for Justice provides a welcoming and intuitive experience."
+
+    const textArea2 = "By the second week of our launch, Relay for Justice had accumulated over 400 active users and 6,000 miles worth of contributed miles."
+
     return(
         <div className="container-fluid dashboard-body">
             <div className="container-fluid relay-for-justice">
@@ -25,15 +36,47 @@ const RelayForJustice = () => {
                 </div>
                 <div className="row text-center relay-banner">
                     <div className="col-12 relay-banner-col">
-                        <img className="w-100 rounded" src={RelayLogo} alt="Relay For Justice Banner"/>
+                        <img className="d-block w-100 rounded" src={RelayBanner} alt="Relay For Justice Banner"/>
                     </div>
                 </div>
-                <div className="row relay-section">
-                    <div className="col-xs-12 col-md-8 text-left">
-                        <p>Hosted by Legal Aid Chicago’s Ambassadors Board, Relay for Justice is a virtual walk, run, and bike event that will connect and energize each of us, as we continue our daily advocacy for civil justice in the current much-changed state of the world.</p>
+                <div className="row align-items-center">
+                    <div className="col-12 col-md-4 large-phone-container">
+                        <img className="d-block w-100" src={RFJmobile4} alt="Relay for Justice mobile screenshot" />
                     </div>
-                    <div className="col-xs-12 col-md-4">
-                        Image Goes Here
+                    <div className="col-12 col-md-8 relay-intro-text">
+                        <h2>About</h2>
+                        <p>Hosted by <ExternalLink className="rfj-link" href="https://www.legalaidchicago.org/">Legal Aid Chicago</ExternalLink>’s Ambassadors Board, Relay for Justice is a virtual walk, run, and bike event that connects and energizes each of us, as we continue our daily advocacy for civil justice in the current much-changed state of the world.</p>
+                        <p>{textArea1}</p>
+                        <p>{textArea2}</p>
+                    </div>
+                </div>
+                <div className="row align-items-center">
+                    <div className="col-12 col-md-4 text-left relay-text">
+                        <h2>Join Us</h2>
+                        <p>Visit our site at <ExternalLink className="rfj-link" href="https://relayforjustice.com/">Relayforjustice.com</ExternalLink>. Invite your friends and family to participate in our next relay!</p>
+                    </div>
+                    <div className="col-12 col-md-8 rfj-desktop-container">
+                        <img className="d-block w-100 rounded rfj-desktop" src={RFJdesktop} alt="Relay for Justice desktop screenshot" />
+                    </div>
+                </div>
+                <div className="row phone-row justify-content-center">
+                    <div className="col-12 col-md-3 phone-container">
+                        <img className="d-block w-100" src={RFJmobile1} alt="Relay for Justice mobile screenshot" />
+                    </div>
+                    <div className="col-12 col-md-3 phone-container">
+                        <img className="d-block w-100" src={RFJmobile5} alt="Relay for Justice mobile screenshot" />
+                    </div>
+                    <div className="col-12 col-md-3 phone-container">
+                        <img className="d-block w-100" src={RFJmobile3} alt="Relay for Justice mobile screenshot" />
+                    </div>
+                </div>
+                <div className="row align-items-center text-left">
+                    <div className="col-12 col-md-6 relay-testimonial-text">
+                        <h2>Testimonial</h2>
+                        <p>Big thank you to Legal Aid for their positive feedback and support. Shout out to my fellow devolopers at Chi-IT who made this happen.</p>
+                    </div>
+                    <div className="col-12 col-md-6 relay-testimonial-container">
+                        <img className="d-block w-100" src={RFJtestimonial} alt="Relat for Justice testimonial" />
                     </div>
                 </div>
             </div>
